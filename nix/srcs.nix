2f27c488498056eff0d502fab093471bae34e291
tomo <tomo@crowdeo.com>
2025-12-01 21:12:18 +0700

    compat: bump makerpkgs after semver-range override

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "e161765790732a9b58e0f3bc7f31308f4949d239";
+    rev = "f1af4a4b9fae8cb5d02e1e1f2c7459ed7d1c065f";
     ref = "compat/1.13.0-semver";
   };
 }
