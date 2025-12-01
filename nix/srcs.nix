bef8995ea8cba5cc66500fbc67be6aacb9c57fff
tomo <tomo@crowdeo.com>
2025-12-01 18:10:11 +0700

    compat: bump makerpkgs after solidity-package/test-hevm fix

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "7b07ec0d6c504f42ede4a77e3a80a2a2516b463b";
+    rev = "12f3297ccc312dc89ad8ff4ea73d976691898739";
     ref = "compat/1.13.0-semver";
   };
 }
