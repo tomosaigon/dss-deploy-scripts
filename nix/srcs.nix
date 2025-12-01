146c343e5b4d3929a77671d3f80a92b1158c633b
tomo <tomo@crowdeo.com>
2025-12-01 20:23:09 +0700

    compat: bump makerpkgs after disabling hevm tests

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "12f3297ccc312dc89ad8ff4ea73d976691898739";
+    rev = "66b5d406e42b0a7391e2e313d769b34236acb3c2";
     ref = "compat/1.13.0-semver";
   };
 }
