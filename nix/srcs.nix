e329308ac0c1a2f03713b194c1fae253f8db992d
tomo <tomo@crowdeo.com>
2025-12-01 22:13:45 +0700

    compat: bump makerpkgs after fixing haskellPackages override

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "71239f26d4a817e516a5d2a20f6678e5c9d29da7";
+    rev = "a96d715d15c1e72aa7bddc023a738420e13bf8b3";
     ref = "compat/1.13.0-semver";
   };
 }
