b8dc4c9cf6425f619cbb8647acd40f09db4967e8
tomo <tomo@crowdeo.com>
2025-12-01 09:25:44 +0700

    compat: point makerpkgs to semver-fixed commit

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -1,8 +1,8 @@
 rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
-    url = "https://github.com/makerdao/makerpkgs";
-    rev = "1a4820a5a4438046c0acbff3375570fe1d060bc4";
-    ref = "master";
+    url = "https://github.com/tomosaigon/makerpkgs";
+    rev = "d51cbcba9024ba4010e75a8e34c3cc68bde13b42";
+    ref = "compat/1.13.0-semver";
   };
 }
