a460f2889a6c715d2dd9072125cb08e4b2b78347
tomo <tomo@crowdeo.com>
2025-12-01 18:03:25 +0700

    compat: bump makerpkgs to dapptools semver-clean commit

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "26dd5b24a587041b24a3a1e8a56fb86547739ba2";
+    rev = "7b07ec0d6c504f42ede4a77e3a80a2a2516b463b";
     ref = "compat/1.13.0-semver";
   };
 }
