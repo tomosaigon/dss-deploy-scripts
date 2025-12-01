59ee0617c7258f6f9eac01a404963126833076af
tomo <tomo@crowdeo.com>
2025-12-01 17:49:11 +0700

    compat: bump makerpkgs after dapptools tarball fix

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "787a0ad9579271ddb0eb1d43796ce76f1387754a";
+    rev = "ac588f408fa7ce0af6abb9ca124a784c8665fcf9";
     ref = "compat/1.13.0-semver";
   };
 }
