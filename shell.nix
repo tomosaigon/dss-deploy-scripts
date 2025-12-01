b32b33f037a21a61915e64a767f45642b045d9c2
tomo <tomo@crowdeo.com>
2025-12-01 12:00:22 +0700

    compat: locally override semver-range to Hackage in shell

--- shell.nix
+++ shell.nix
@@ -1,14 +1,43 @@
-let srcs = import ./nix/srcs.nix; in
+let
+  srcs = import ./nix/srcs.nix;
+in
+{ dapptoolsOverrides ? {}
+, pkgs ?
+    let
+      # Original makerpkgs import
+      basePkgs = import srcs.makerpkgs {
+        inherit dapptoolsOverrides;
+      };
 
-{ pkgs ? import srcs.makerpkgs { inherit dapptoolsOverrides; }
-, dapptoolsOverrides ? {}
+      # Local overlay to force semver-range to come from Hackage
+      pkgsWithSemverFix = basePkgs.extend (self: super: {
+        haskellPackages = super.haskellPackages.override (old: {
+          overrides = super.lib.composeExtensions
+            (old.overrides or (_: _: {}))
+            (self-hs: super-hs: {
+              semver-range =
+                (super-hs.semver-range.override {})
+                  .overrideAttrs (oldAttrs: {
+                    src = super.fetchurl {
+                      url =
+                        "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
+                      sha256 =
+                        "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
+                    };
+                  });
+            });
+        });
+      });
+    in pkgsWithSemverFix
 , doCheck ? false
 , githubAuthToken ? null
-}@args: with pkgs;
+}@args:
+with pkgs;
 
 let
   dds = import ./. args;
-in mkShell {
+in
+mkShell {
   buildInputs = dds.bins ++ [
     dds
     dapp2nix
