cb36e3de80cd5e5defc0b7ee8103722b2d3e46f8
tomo <tomo@crowdeo.com>
2025-12-02 11:43:34 +0700

    compat: bump makerpkgs after fetchzip URL override

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "4e44f84f67a90f69f3b76a4b2cacc05e6756b4a9";
+    rev = "c4f56a3c685471baab42764697212f4a78cf7c12";
     ref = "compat/1.13.0-semver";
   };
 }
