5eba050abd0601fe681ebea8877b3442d13ed8a2
tomo <tomo@crowdeo.com>
2025-12-03 12:08:14 +0700

    nix: pin makerpkgs to compat/1.13.0-semver rev 474c1d0
    Use tomosaigon/makerpkgs@474c1d05 as the source for makerpkgs.
    This revision contains the semver-range 0.2.8 / Hackage fixes we need,
    instead of relying on the upstream broken semver-range patch-1 source.

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -2,7 +2,7 @@ rec {
   # Default import pinned pkgs
   makerpkgs = fetchGit {
     url = "https://github.com/tomosaigon/makerpkgs";
-    rev = "b9ff07530e1334f972dfe2815a74923f68241676";
+    rev = "474c1d0577aab05e34ffde2874e96f1794f6f3ea";
     ref = "compat/1.13.0-semver";
   };
 }
